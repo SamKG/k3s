9784199dde5c1f6640720dd13b9cea7bcfa50977
SamKG <samyak.k.gupta@gmail.com>
2025-06-09 00:44:07 -0400

    Remove dependence on net in rootless mode
    Signed-off-by: SamKG <samyak.k.gupta@gmail.com>

--- pkg/rootless/rootless.go
+++ pkg/rootless/rootless.go
@@ -97,12 +97,12 @@ func validateSysctl() error {
 		"net.ipv4.ip_forward": "1",
 	}
 	for key, expectedValue := range expected {
-		if actualValue, err := readSysctl(key); err == nil {
-			if expectedValue != actualValue {
-				return fmt.Errorf("expected sysctl value %q to be %q, got %q; try adding \"%s=%s\" to /etc/sysctl.conf and running `sudo sysctl --system`",
-					key, expectedValue, actualValue, key, expectedValue)
-			}
-		}
+		// if actualValue, err := readSysctl(key); err == nil {
+		// 	if expectedValue != actualValue {
+		// 		return fmt.Errorf("expected sysctl value %q to be %q, got %q; try adding \"%s=%s\" to /etc/sysctl.conf and running `sudo sysctl --system`",
+		// 			key, expectedValue, actualValue, key, expectedValue)
+		// 	}
+		// }
 	}
 	return nil
 }
